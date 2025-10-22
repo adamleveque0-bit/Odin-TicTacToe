c044923e9078970f29e1d88144679ff492a6a5c7
adamleveque91 <adam.leveque0@gmail.com>
2025-10-22 14:35:24 -0500

    begin the logic of the game

--- main.js
+++ main.js
@@ -1,1 +1,35 @@
 console.log("im here!");
+
+let results = "";
+let currentGame = [];
+let X = [];
+let O = [];
+let winningpatterns =[
+            [0, 1, 2],
+            [0, 3, 6],
+            [0, 4, 8],
+            [1, 4, 7],
+            [2, 5, 8],
+            [2, 4, 6],
+            [3, 4, 5],
+            [6, 7, 8]
+];
+
+const game1 = [ 'x','o','x',
+                'x','o','x',
+                'o','x','o',];
+
+const game2 = [ 'o','o','x',
+                'x','o','x',
+                'o','x','o',];
+
+function CheckWinner(game, results){
+    let winorlose;
+
+    
+
+    return winorlose;
+}
+
+CheckWinner(game1);
+CheckWinner(game2)
