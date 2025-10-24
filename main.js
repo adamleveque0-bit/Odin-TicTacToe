438e4fed453288b56d62d8e74f80894bb7c72b08
adamleveque91 <adam.leveque0@gmail.com>
2025-10-24 13:30:20 -0500

    im starting to know what im doing

--- main.js
+++ main.js
@@ -1,22 +1,40 @@
 console.log("im here!");
 
 let results = "";
-let currentGame = [];
+let currentGameX = [];
+let currentGameO = [];
+let getButtons = document.querySelectorAll('.box');
+let getTurn = document.querySelector('header p');
 
+for(let i = 0; i < getButtons.length; i++){
+    getButtons[i].addEventListener('click', ()=>{
+    console.log('you clicked me!');
+        if(getButtons[i].innerHTML == ''){
 
+            if(getTurn.innerHTML == 'x'){
+                getButtons[i].innerHTML = 'x';
+                getTurn.innerHTML = 'o';
+                currentGameX[i] += i;
+                //CheckWinner(currentGame, results);
+            }else if(getTurn.innerHTML = 'o'){
+                getButtons[i].innerHTML = 'o';
+                getTurn.innerHTML = 'x';
+                currentGameO[i] += i;
+                //CheckWinner(currentGame, results);
+            }else{
+                alert('uh oh something is wrong');
+            }
 
-const game1 = [ 'x','o','x',
-                'x','o','x',
-                'o','x','o',];
-
-const game2 = [ 'o','o','x',
-                'x','o','x',
-                'o','x','o',];
+        }else{
+            alert('you already clicked that one!');
+        }
+        console.log('this is o board', currentGameO);
+        console.log('this is x board', currentGameX);
+    });
+}
 
 function CheckWinner(game, results){
     let winorlose;
-    let X = [];
-    let O = [];
     let winningpatterns =[
             [0, 1, 2],
             [0, 3, 6],
@@ -27,16 +45,12 @@ function CheckWinner(game, results){
             [3, 4, 5],
             [6, 7, 8]];
 
-    for(let i = 0; i > currentGame.length; i++){
-        if(currentGame[i].innerHtml = 'x'){
-
-        }else if(currentGame[i].innerHtml = '0'){
-
-        }
+    for(let i = 0; i > game.length; i++){
+        
     }
 
     return results;
 }
 
-CheckWinner(game1);
-CheckWinner(game2)
+//CheckWinner(game1);
+//CheckWinner(game2);
