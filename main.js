991e9a7d93232acd085469a375ddadee79c6f9e1
adamleveque91 <adam.leveque0@gmail.com>
2025-10-24 12:39:52 -0500

    i dont know what im doing

--- main.js
+++ main.js
@@ -2,18 +2,8 @@ console.log("im here!");
 
 let results = "";
 let currentGame = [];
-let X = [];
-let O = [];
-let winningpatterns =[
-            [0, 1, 2],
-            [0, 3, 6],
-            [0, 4, 8],
-            [1, 4, 7],
-            [2, 5, 8],
-            [2, 4, 6],
-            [3, 4, 5],
-            [6, 7, 8]
-];
+
+
 
 const game1 = [ 'x','o','x',
                 'x','o','x',
@@ -25,10 +15,27 @@ const game2 = [ 'o','o','x',
 
 function CheckWinner(game, results){
     let winorlose;
+    let X = [];
+    let O = [];
+    let winningpatterns =[
+            [0, 1, 2],
+            [0, 3, 6],
+            [0, 4, 8],
+            [1, 4, 7],
+            [2, 5, 8],
+            [2, 4, 6],
+            [3, 4, 5],
+            [6, 7, 8]];
 
-    
+    for(let i = 0; i > currentGame.length; i++){
+        if(currentGame[i].innerHtml = 'x'){
 
-    return winorlose;
+        }else if(currentGame[i].innerHtml = '0'){
+
+        }
+    }
+
+    return results;
 }
 
 CheckWinner(game1);
