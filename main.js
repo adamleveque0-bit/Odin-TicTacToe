c0ed7d90c1acaaf916d66cf11413ea3f4578e78a
adamleveque91 <adam.leveque0@gmail.com>
2025-10-26 12:46:27 -0500

    The base game is done it could use some refinement but the hard part is over

--- main.js
+++ main.js
@@ -3,62 +3,66 @@ console.log("im here!");
 let results = "";
 let currentGameX = [];
 let currentGameO = [];
+let whosTurn = false; // false is O and True is X
+let whoWon = [false, false];
 let getButtons = document.querySelectorAll('.box');
 let getTurn = document.querySelector('header p');
 let regex = /,/g;
 
 for(let i = 0; i < getButtons.length; i++){
     getButtons[i].addEventListener('click', ()=>{
-        if(getButtons[i].innerHTML == ''){
+        if(whoWon[0] == false && whoWon[1] == false){
 
-            if(getTurn.innerHTML == 'x'){
-                getButtons[i].innerHTML = 'x';
-                getTurn.innerHTML = 'o';
-                currentGameX[i] += i.toString();
-                fixBoard();
-                CheckWinner(currentGameX);
-            }else if(getTurn.innerHTML = 'o'){
-                getButtons[i].innerHTML = 'o';
-                getTurn.innerHTML = 'x';
-                currentGameO[i] += i;
-                //CheckWinner(currentGameO);
-            }else{
-                alert('uh oh something is wrong');
+            if(getButtons[i].innerHTML == ''){
+
+                if(getTurn.innerHTML == 'x'){
+                    getButtons[i].innerHTML = 'x';
+                    getTurn.innerHTML = 'o';
+                    whosTurn = true;
+                    currentGameX[i] += i.toString();
+                    fixBoard();
+                    CheckWinner(currentGameX);
+                }else if(getTurn.innerHTML = 'o'){
+                    getButtons[i].innerHTML = 'o';
+                    getTurn.innerHTML = 'x';
+                    whosTurn = false;
+                    currentGameO[i] += i.toString();
+                    fixBoard();
+                    CheckWinner(currentGameO);
+                }else{
+                    alert('uh oh something is wrong');
             }
 
-        }else{
-            alert('you already clicked that one!');
+            }else{
+                alert('you already clicked that one!');
+            }
+    }
+
+        if(whoWon[0] == true){
+            getTurn.innerHTML = "X Wins!";
         }
-        //console.log('this is o board', currentGameO);
-        //console.log('this is x board', currentGameX.toString());
+        if(whoWon[1] == true){
+            getTurn.innerHTML = "O Wins!";
+        }
         
     });
 }
 
 function CheckWinner(game){
     let winorlose;
-    let testcase = '012';
     let curgame = game.toString();
-    console.log(curgame);
     curgame = curgame.replace(regex, '');
-    console.log(curgame);
 
-    let winningpatterns =[
-            [/012/g],
-            [/036/g],
-            [/048/g],
-            [/147/g],
-            [/258/g],
-            [/246/g],
-            [/345/g],
-            [/678/g]];
+    let winningpatterns =[/012/g,/036/g,/048/g,/147/g,/258/g,/246/g,/345/g,/678/g];
     
-    
-    winorlose = winningpatterns[0].test(testcase);
-    console.log(winorlose);
     for(let i = 0;i < winningpatterns.length; i++){
 
-        if(curgame.match(winningpatterns[i])){
+        if(winningpatterns[i].test(curgame)){
+            if(whosTurn == false){
+                whoWon[1] = true;
+            }else if(whosTurn == true){
+                whoWon[0] = true;
+            }
             console.log('you win');
         }else{
             console.log('no winner yet');
@@ -73,13 +77,13 @@ function fixBoard(){
             currentGameX[i] = currentGameX[i].replace('undefined','');
         }
     }
+    for(let i = 0; i < currentGameO.length; i++){
+        if(typeof currentGameO[i] == 'string'){
+            currentGameO[i] = currentGameO[i].replace('undefined','');
+        }
+    }
 }
-function gameConvert(game){
 
-
-
-    return game;
-}
 
 //CheckWinner(game1);
 //CheckWinner(game2);
